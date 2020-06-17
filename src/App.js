dd683e723d080459c33d224fbfde0fca8ec554f7
Segy Hendro Pratama <segy.hendro@mekari.com>
2020-06-17 20:34:09 +0700

    feat: apply code splitting

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 import { Component } from 'react';
 
 import Page1 from './components/Page1';
-import Page2 from './components/Page2';
-import Page3 from './components/Page3';
+
+//do not the import statement again if we use code splitting
+// import Page2 from './components/Page2';
+// import Page3 from './components/Page3';
 
 
 
@@ -13,22 +15,41 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      route: 'page1'
+      route: 'page1',
+      component: '',
     }
   }
 
   onRouteChange = (route) => {
-    this.setState({route})
+    //Without  code splitting
+    // this.setState({route})
+    //With code Splitting
+    if(route === 'page1') {
+      this.setState({route});
+    } else if(route === 'page2') {
+      import('./components/Page2').then((Page2) => {
+        this.setState({route, component: Page2.default});
+      })
+    } else if(route === 'page3') {
+      import('./components/Page3').then((Page3) => {
+        this.setState({route, component: Page3.default});
+      })
+    }
   }
 
   render() {
-      if(this.state.route === 'page1') {
-        return <Page1 onRouteChange={this.onRouteChange} />
-      } else if(this.state.route === 'page2') {
-        return <Page2 onRouteChange={this.onRouteChange} />
-      } else {
-        return <Page3 onRouteChange={this.onRouteChange}/>
-      }
+      // if(this.state.route === 'page1') {
+      //   return <Page1 onRouteChange={this.onRouteChange} />
+      // } else if(this.state.route === 'page2') {
+      //   return <Page2 onRouteChange={this.onRouteChange} />
+      // } else {
+      //   return <Page3 onRouteChange={this.onRouteChange}/>
+      // }
+            if(this.state.route === 'page1') {
+              return <Page1 onRouteChange={this.onRouteChange} />
+            } else {
+              return <this.state.component onRouteChange={this.onRouteChange} />
+            }
   }
 }
 
